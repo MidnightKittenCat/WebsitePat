9d337c4a8f4efbc3fc3c601f96fc9b1813c97447
MidnightKittenCat <66114416+MidnightKittenCat@users.noreply.github.com>
2023-12-01 19:09:24 +1100

    Update route.tsx

--- src/app/count/route.tsx
+++ src/app/count/route.tsx
@@ -2,6 +2,7 @@ import { createClient } from "@vercel/kv";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+  cookies()
   const counter = createClient({
     url: process.env.KV_REST_API_URL!,
     token: process.env.KV_REST_API_TOKEN!,

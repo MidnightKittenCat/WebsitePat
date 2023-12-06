9719a0282fe4dd407fdaf1fda6adeacccd111220
MidnightKittenCat <66114416+MidnightKittenCat@users.noreply.github.com>
2023-12-06 23:41:24 +1100

    Update route.tsx

--- src/app/route.tsx
+++ src/app/route.tsx
@@ -3,6 +3,51 @@ import { cookies } from "next/headers";
 import { createClient } from "@vercel/kv";
 import { NextResponse } from "next/server";
 
+const pageStyles = `
+  body {
+    margin: 0;
+    overflow: hidden;
+    font-family: system-ui, sans-serif, serif;
+  }
+
+  main {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: center / cover no-repeat url('/headpat.gif');
+  }
+
+  .popup {
+    background: #00000070;
+    border-radius: 0.5em;
+    padding: 1.5em 2em;
+    color: white;
+    font-size: 1.25em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .popup-message {
+    margin: 0;
+  }
+
+  .counter {
+    margin: 0;
+    margin-top: 0;
+    margin-bottom: 0;
+    margin-left: auto;
+    margin-right: auto;
+    background: #66d74a87;
+    border-radius: 0.5em;
+    padding: 0.1em 0.5em 0.3em 0.5em;
+    display: inline-block;
+  }
+`;
+
 export async function GET() {
   const cookieStore = cookies();
   const lastHeadpat = cookieStore.get("headpat");
@@ -12,130 +57,54 @@ export async function GET() {
   });
 
   let didTheDeed = false;
-
   let current = (await counter.get<number>("headpats")) ?? 0;
-  const isWithin24Hours =
-    !lastHeadpat ||
-    new Date(lastHeadpat.value).getTime() > new Date().getTime() - 1000 * 60 * 60 * 24;
 
-  if (isWithin24Hours) {
+  if (
+    !lastHeadpat ||
+    new Date(lastHeadpat.value).getTime() >
+      new Date().getTime() - 1000 * 60 * 60 * 24
+  ) {
     cookieStore.set("headpat", new Date().getTime().toString(), {
       httpOnly: true,
       expires: new Date().getTime() + 1000 * 60 * 60 * 24,
       secure: true,
     });
-
     current++;
     didTheDeed = true;
     await counter.set("headpats", current);
   }
 
-  const commonStyles = `
-    margin: 0;
-    overflow: hidden;
-    font-family: system-ui, sans-serif, serif;
-  `;
+  const counterSuffix =
+    current === 1 ? "st" : current === 2 ? "nd" : current === 3 ? "rd" : "th";
 
-  if (!didTheDeed) {
-    return new NextResponse(
-      `
-      <html lang="en">
-        <head>
-          <title>Midnight's Headpat</title>
-        </head>
-        <body style="${commonStyles}">
-          <main style="
-            width: 100vw;
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background: center / cover no-repeat url('/pout.gif');
-          ">
-            <div style="
-              background: #00000070;
-              border-radius: 0.5em;
-              padding: 1.5em 2em;
-              color: white;
-              font-size: 1.25em;
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              flex-direction: column;
-            ">
-              <div style="margin: 0;">Hey! <i>hmph</i> That's enough patting for today.</div>
-            </div>
-          </main>
-        </body>
-      </html>
-      `,
-      { headers: { "Content-Type": "text/html" } }
-    );
-  }
-
-  return new NextResponse(
-    `
-    <html lang="en">
+  const responseHtml = `
+    <html dir="ltr" lang="en">
       <head>
         <title>Midnight's Headpat</title>
+        <style>${pageStyles}</style>
       </head>
-      <body style="${commonStyles}">
-        <main style="
-          width: 100vw;
-          height: 100vh;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background: center / cover no-repeat url('/headpat.gif');
-        ">
-          <div style="
-            background: #00000070;
-            border-radius: 0.5em;
-            padding: 1.5em 2em;
-            color: white;
-            font-size: 1.25em;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-          ">
-            <div style="margin: 0;">
-              You are the ${current}${getOrdinalSuffix(current)}
-            </div>
-            <div style="
-              margin-top: 0;
-              margin-bottom: 0;
-              margin-left: auto;
-              margin-right: auto;
-              background: #66d74a87;
-              border-radius: 0.5em;
-              padding: 0.1em 0.5em 0.3em 0.5em;
-              display: inline-block;
-            ">
-              patter
-            </div>
+      <body>
+        <main>
+          <div class="popup">
+            ${didTheDeed ? `
+              <div class="popup-message">
+                You are the ${current}${counterSuffix}
+              </div>
+              <div class="counter">patter</div>
+            ` : `
+              <div class="popup-message">
+                Hey! <i>hmph</i> That's enough patting for today.
+              </div>
+            `}
           </div>
         </main>
       </body>
     </html>
-    `,
-    { headers: { "Content-Type": "text/html" } }
-  );
+  `;
+
+  return new NextResponse(responseHtml, {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
-
-
-function getOrdinalSuffix(number: number | string): string {
-  const numericNumber = typeof number === 'string' ? parseInt(number, 10) : number;
-
-  if (isNaN(numericNumber)) {
-    // Handle the case where the number is not a valid numeric value.
-    return '';
-  }
-
-  const suffixes = ["th", "st", "nd", "rd"];
-  const remainder = numericNumber % 100;
-  return suffixes[(remainder - 20) % 10] || suffixes[remainder] || suffixes[0];
-}
-
-
-

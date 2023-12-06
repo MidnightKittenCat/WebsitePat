29a480597d97924a8141b5d665429dd7a2d8f794
MidnightKittenCat <66114416+MidnightKittenCat@users.noreply.github.com>
2023-12-06 23:23:16 +1100

    Update route.tsx

--- src/app/route.tsx
+++ src/app/route.tsx
@@ -10,66 +10,121 @@ export async function GET() {
     url: process.env.KV_REST_API_URL!,
     token: process.env.KV_REST_API_TOKEN!,
   });
+
   let didTheDeed = false;
 
   let current = (await counter.get<number>("headpats")) ?? 0;
-  if (
+  const isWithin24Hours =
     !lastHeadpat ||
-    new Date(lastHeadpat.value).getTime() >
-      new Date().getTime() - 1000 * 60 * 60 * 24
-  ) {
+    new Date(lastHeadpat.value).getTime() > new Date().getTime() - 1000 * 60 * 60 * 24;
+
+  if (isWithin24Hours) {
     cookieStore.set("headpat", new Date().getTime().toString(), {
       httpOnly: true,
       expires: new Date().getTime() + 1000 * 60 * 60 * 24,
       secure: true,
     });
+
     current++;
     didTheDeed = true;
     await counter.set("headpats", current);
   }
 
+  const commonStyles = `
+    margin: 0;
+    overflow: hidden;
+    font-family: system-ui, sans-serif, serif;
+  `;
+
   if (!didTheDeed) {
     return new NextResponse(
-      `<html dir="ltr" lang="en">
-  <head>
-    <title>Midnight's Headpat</title>
-  </head>
-  <body style="margin:0;overflow:hidden;font-family:system-ui,sans-serif,serif;">
-    <main style="width:100vw;height:100vh;display:flex;justify-content:center;align-items:center;background:center / cover no-repeat url('/pout.gif');">
-      <div style="background:#00000070;border-radius:0.5em;padding:1.5em 2em;color:white;font-size:1.25em;display:flex;justify-content:center;align-items:center;flex-direction:column;">
-        <div style="margin:0;">Hey! <i>hmph</i> That's enough patting for today.</div>
-      </div>
-    </main>
-  </body>
-</html>`,
-      {
-        headers: {
-          "Content-Type": "text/html",
-        },
-      }
+      `
+      <html lang="en">
+        <head>
+          <title>Midnight's Headpat</title>
+        </head>
+        <body style="${commonStyles}">
+          <main style="
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: center / cover no-repeat url('/pout.gif');
+          ">
+            <div style="
+              background: #00000070;
+              border-radius: 0.5em;
+              padding: 1.5em 2em;
+              color: white;
+              font-size: 1.25em;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              flex-direction: column;
+            ">
+              <div style="margin: 0;">Hey! <i>hmph</i> That's enough patting for today.</div>
+            </div>
+          </main>
+        </body>
+      </html>
+      `,
+      { headers: { "Content-Type": "text/html" } }
     );
   }
 
   return new NextResponse(
-    `<html dir="ltr" lang="en">
-  <head>
-    <title>Midnight's Headpat</title>
-  </head>
-  <body style="margin:0;overflow:hidden;font-family:system-ui,sans-serif,serif;">
-    <main style="width:100vw;height:100vh;display:flex;justify-content:center;align-items:center;background:center / cover no-repeat url('/headpat.gif');">
-      <div style="background:#00000070;border-radius:0.5em;padding:1.5em 2em;color:white;font-size:1.25em;display:flex;justify-content:center;align-items:center;flex-direction:column;">
-        <div style="margin:0;">You are the ${current}${
-      current === 1 ? "st" : current === 2 ? "nd" : current === 3 ? "rd" : "th"
-    }</div>
-        <div style="margin-top:0;margin-bottom:0;margin-left:auto;margin-right:auto;background:#66d74a87;border-radius:0.5em;padding:0.1em 0.5em 0.3em 0.5em;display:inline-block;">patter</div>
-      </div>
-    </main>
-  </body>
-</html>`,
-    {
-      headers: {
-        "Content-Type": "text/html",
-      },
-    }
+    `
+    <html lang="en">
+      <head>
+        <title>Midnight's Headpat</title>
+      </head>
+      <body style="${commonStyles}">
+        <main style="
+          width: 100vw;
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background: center / cover no-repeat url('/headpat.gif');
+        ">
+          <div style="
+            background: #00000070;
+            border-radius: 0.5em;
+            padding: 1.5em 2em;
+            color: white;
+            font-size: 1.25em;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+          ">
+            <div style="margin: 0;">
+              You are the ${current}${getOrdinalSuffix(current)}
+            </div>
+            <div style="
+              margin-top: 0;
+              margin-bottom: 0;
+              margin-left: auto;
+              margin-right: auto;
+              background: #66d74a87;
+              border-radius: 0.5em;
+              padding: 0.1em 0.5em 0.3em 0.5em;
+              display: inline-block;
+            ">
+              patter
+            </div>
+          </div>
+        </main>
+      </body>
+    </html>
+    `,
+    { headers: { "Content-Type": "text/html" } }
   );
+}
+
+function getOrdinalSuffix(number) {
+  const suffixes = ["th", "st", "nd", "rd"];
+  const remainder = number % 100;
+  return suffixes[(remainder - 20) % 10] || suffixes[remainder] || suffixes[0];
 }
